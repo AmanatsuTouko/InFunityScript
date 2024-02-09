0f0076be686e0d0d822ec4474b252602cf5d2d5b
AmanatsuToko <88881947+AmanatsuTouko@users.noreply.github.com>
2024-02-09 21:05:22 +0900

    Organize script path.

--- Assets/InFunityScript/Scripts/SceneController.cs
+++
@@ -1,20 +0,0 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.SceneManagement;
-
-namespace FThingSoftware.InFunityScript
-{
-    public class SceneController : MonoBehaviour
-    {
-        // Titleに戻る
-        public void ChangeTitleScene()
-        {
-            SceneManager.LoadScene("Title");
-        }
-
-        // ゲームを終了する
-        public void ExitGame()
-        {
-#if UNITY_EDITOR            UnityEditor.EditorApplication.isPlaying = false;
-#else            Application.Quit();
-#endif        }
-    }
-}
